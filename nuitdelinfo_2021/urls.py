3cd8535ab9ae48e98b052578fb1040c22e8a0c0d
flifloo <flifloo@gmail.com>
2021-12-03 01:08:46 +0100

    Implement login/register

--- nuitdelinfo_2021/urls.py
+++ nuitdelinfo_2021/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path("a/", include("rescue.urls")),
     path("p/", include("people.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 handler400 = "error.views.index"
